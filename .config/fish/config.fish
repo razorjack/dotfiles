4d12206a68dbfc0176f225aacd4714514d054a79
Jacek Galanciak <jacek.galanciak@gmail.com>
2018-09-19 16:19:22 +0200

    Tweak fish shell configuration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,10 +14,8 @@ rbenv rehash
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
 
-export HOMEBREW_INSTALL_BADGE="🔫  💪"
-export FZF_DEFAULT_COMMAND='
-  (ag -g "" ||
-   find * -name ".*" -prune -o -type f -print -o -type l -print) 2> /dev/null'
+set -x HOMEBREW_INSTALL_BADGE "🔫  💪"
+set -x FZF_DEFAULT_COMMAND 'ag -g ""'
 
-export GOPATH="$HOME/.golang"
-export HISTCONTROL=ignoreboth:erasedups
+set -x GOPATH "$HOME/.golang"
+set -x HISTCONTROL ignoreboth:erasedups
