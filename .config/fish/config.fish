2bc90149112d43c60ebde3dbc66fec65db6b9aa1
Jacek Galanciak <jacek.galanciak@gmail.com>
2018-12-18 16:17:38 +0100

    Set explicit path for postgres 9

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,6 +11,8 @@ eval (direnv hook fish)
 set -x PATH $HOME/.rbenv/shims $PATH
 rbenv rehash
 
+set -x PATH /usr/local/opt/postgresql@9.6/bin $PATH
+
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
 
